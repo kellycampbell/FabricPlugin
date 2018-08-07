8dbb61d9173670a39a66e0b16ef194444b903fc2
Kelly Campbell <kellyc@bulogics.com>
2018-08-07 10:28:40 -0400

    Don't run after_prepare ios hook unless it's on 'darwin' os
    The cordova platforms list doesn't seem to be populated every time the
    hooks are run so this is a hacky workaround.

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -1,3 +1,4 @@
+var os = require('os');
 
 var androidHelper = require('./lib/android-helper');
 var iosHelper = require("./lib/ios-helper");
@@ -7,14 +8,16 @@ module.exports = function(context) {
     console.log("FABRIC PLUGIN HOOKS RUNNING");
     var platforms = context.opts.cordova.platforms;
 
-    console.log("PLATFORMS = " + JSON.stringify(platforms))
+    // console.log("PLATFORMS = " + JSON.stringify(platforms))
     // Add a build phase which runs a shell script that executes the Crashlytics
     // run command line tool which uploads the debug symbols at build time.
-    var xcodeProjectPath = utilities.getXcodeProjectPath(context);
-    if (xcodeProjectPath) {
-      iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
-      iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
-    } else {
-      console.log("NO XCODE PROJECT PATH IN CONTEXT");
+    if (os.platform() == 'darwin') {
+      var xcodeProjectPath = utilities.getXcodeProjectPath(context);
+      if (xcodeProjectPath) {
+        iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
+        iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
+      } else {
+        console.log("NO XCODE PROJECT PATH IN CONTEXT");
+      }
     }
   };
